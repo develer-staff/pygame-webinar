e729937c1d7b4cc6107b9df46feac003f97a4778
LeoamrosIV <lamirante@develer.com>
2022-11-28 18:26:36 +0100

    Aggiunge scritte con font

--- src/states.py
+++ src/states.py
@@ -179,7 +179,25 @@ class Pause(State):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.screen.fill("darkgreen")
+        self.screen.fill(BLUE)
+
+        # Inizializza font.
+        big_font = pg.font.Font(FONTS / "NormalFont.ttf", 50)
+        small_font = pg.font.Font(FONTS / "NormalFont.ttf", 18)
+
+        # Crea `Surface` con scritta.
+        bf_surf = big_font.render("PAUSA", False, YELLOW)
+        sf_surf = small_font.render("ESC: TORNA AL GIOCO", False, YELLOW)
+
+        # Creo `Rect` per la posizione.
+        x, y = VIEW_RES
+        center_x, center_y = x / 2, y / 2
+        bf_rect = bf_surf.get_rect(midbottom=(center_x, center_y))
+        sf_rect = sf_surf.get_rect(midbottom=(center_x, y - 20))
+
+        # Disegno le surface su `self.screen`
+        self.screen.blit(bf_surf, bf_rect)
+        self.screen.blit(sf_surf, sf_rect)
 
     def process_event(self, event: pg.event.Event, dt: int):
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
@@ -191,6 +209,24 @@ class GameOver(State):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.screen.fill(RED)
+
+        # Inizializza font.
+        big_font = pg.font.Font(FONTS / "NormalFont.ttf", 30)
+        small_font = pg.font.Font(FONTS / "NormalFont.ttf", 16)
+
+        # Crea `Surface` con scritta.
+        bf_surf = big_font.render("GAME OVER", False, "white")
+        sf_surf = small_font.render("SPAZIO: NUOVA PARTITA", False, "white")
+
+        # Creo `Rect` per la posizione.
+        x, y = VIEW_RES
+        center_x, center_y = x / 2, y / 2
+        bf_rect = bf_surf.get_rect(midbottom=(center_x, center_y))
+        sf_rect = sf_surf.get_rect(midbottom=(center_x, y - 20))
+
+        # Disegno le surface su `self.screen`
+        self.screen.blit(bf_surf, bf_rect)
+        self.screen.blit(sf_surf, sf_rect)
 
     def process_event(self, event: pg.event.Event, dt: int):
         if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
